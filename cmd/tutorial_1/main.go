194684ca7e96c24f00e8cf006811d2ace73161f6
ShamarB <cs635d@gmail.com>
2025-05-25 16:41:49 -0400

    Update go.mod with some new functions

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var intNum int = 28
 	fmt.Println(intNum)
+
+	myVar := "text"
+	fmt.Println(myVar)
+
+	var mybool bool = true
+	fmt.Println(mybool)
+
+	const pi float64 = 3.14159
+	fmt.Println(pi)
+
+	printMe("Hello")
+
+	numerator := 11
+	denominator := 2
+	var result, remainder int = integerDivision(numerator, denominator)
+	fmt.Printf("The result of integer division is %v with a remainder of %v", result, remainder)
+}
+
+func printMe(printValue string) {
+	fmt.Println("Print Me")
+	fmt.Println(printValue)
+}
+
+func integerDivision(numerator int, denominator int) (int, int) {
+	var result int = numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder
+}
+
+func printMeInt(printValue int) {
+	fmt.Println(printValue)
 }
