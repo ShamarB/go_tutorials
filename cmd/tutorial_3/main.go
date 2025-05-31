b32027ab63a3413391b980354711a5b3fae1f5bd
Shamar Billingslea <shamarbillingslea@Shamars-Mac-mini.local>
2025-05-30 22:00:27 -0400

    Add tutorial 3 for arrays

---
+++ cmd/tutorial_3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num := 0
+
+	switch num {
+	case 0:
+		fmt.Println("Number not found")
+	case 1:
+		fmt.Println("Number found")
+	case 2:
+		fmt.Println("Number not found")
+	}
+}
